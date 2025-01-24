0e17b83f0e443b171551781410a2b61efaccfce1
e-arndt <earndt1701@gmail.com>
2025-01-24 00:00:01 -0800

    Thought I had pushed this, maybe not..

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -18,6 +18,7 @@ public class Basketball
     public static void Run()
     {
         var players = new Dictionary<string, int>();
+        var playerFound = new HashSet<string>();
 
         using var reader = new TextFieldParser("basketball.csv");
         reader.TextFieldType = FieldType.Delimited;
@@ -27,10 +28,57 @@ public class Basketball
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+
+            if (playerFound.Contains(playerId))
+            {
+                players[playerId] += points;
+            }
+            else
+            {
+                playerFound.Add(playerId);
+                players[playerId] = points;
+            }
+            
         }
 
-        Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
+        var topPlayers = players.OrderByDescending(p => p.Value).Take(10).ToArray();
 
-        var topPlayers = new string[10];
+        // var topPlayers = new string[10];
+
+        Console.WriteLine("Players with Highest Career Points:");
+        foreach (var player in topPlayers)
+        {
+            Console.WriteLine($"Player: {player.Key} - {player.Value:N0}");
+        }
+
+
+        // Using topPlayers
+        // for (int i = 0; i < topPlayersList.Length; i++)
+        // {
+        //     topPlayers[i] = $"Player {topPlayersList[i].Key}: {topPlayersList[i].Value} points";
+        // }
+
+        // Console.WriteLine("Top 10 Players: ");
+        // foreach (var player in topPlayers)
+        // {
+        //     Console.WriteLine(player);
+        // }
+
+        // var specificPlayerId = "abramjo01";
+
+
+        // Test one specific player
+        // if (players.ContainsKey(specificPlayerId))
+        // {
+        //     // Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
+        //     Console.WriteLine($"Player {specificPlayerId}: {players[specificPlayerId]} points");
+        // }
+        // else
+        // {
+        //     Console.WriteLine($"Player: {specificPlayerId} not found");
+        // }
+        
+
+        
     }
 }
--- week03/teach/Program.cs
+++ week03/teach/Program.cs
@@ -2,11 +2,11 @@
 // Console.WriteLine("\n======================\nUnique Letters\n======================");
 // UniqueLetters.Run();
 
-Console.WriteLine("\n======================\nDisplay Sums of 10\n======================");
-DisplaySums.Run();
+// Console.WriteLine("\n======================\nDisplay Sums of 10\n======================");
+// DisplaySums.Run();
 
-// Console.WriteLine("\n======================\nBasketball\n======================");
-// Basketball.Run();
+Console.WriteLine("\n======================\nTop NBA Basketball\n======================");
+Basketball.Run();
 
 // Uncomment and run as you get to the solution part
 // Console.WriteLine("\n======================\nUnique Letters\n======================");
